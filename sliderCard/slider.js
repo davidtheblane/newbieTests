9a523338a52f9ed60028fe219cc9dbb2a2b6969d
davidtheblane <davidtheblane@gmail.com>
2021-05-19 20:50:00 -0300

    mounted structure

---
+++ sliderCard/slider.js
@@ -0,0 +1,13 @@
+const boxContainer = document.querySelector(".box__container");
+const miniBox = boxContainer.children;
+const boxContainerWidth = boxContainer.offsetWidth;
+
+const margin = 30;
+var items = 0;
+
+// Responsive
+responsive[
+  ({ breakpoint: { width: 0, item: 1 } },
+  { breakpoint: { width: 600, item: 2 } },
+  { breakpoint: { width: 1000, item: 4 } })
+];
